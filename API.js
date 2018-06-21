9bab6741992c14616417873af9e143720a988983
rpinto <rachel.lea.pinto@gmail.com>
2018-06-21 13:25:02 -0700

    made html markup

--- API.js
+++ API.js
@@ -1,3 +1,15 @@
 'use strict';
 
 /* global store, $, api */
+
+const api = (function() {
+  const BASE_URL = "https://thinkful-list-api.herokuapp.com/rpinto/bookmarks";
+
+  
+  
+  
+  
+  return {
+
+  };
+}());
