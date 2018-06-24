b1702318c606468233b40d06681445973eeb5a2a
rpinto <rachel.lea.pinto@gmail.com>
2018-06-24 11:25:34 -0700

    filter working on click, not on select. cleaned up code.

--- STORE.js
+++ STORE.js
@@ -18,6 +18,7 @@ const store = (function(){
     error: false,
     addItem,
     findAndDelete,
+    minRating: 1,
   };
 
 }());
--- bookmarks.js
+++ bookmarks.js
@@ -6,7 +6,6 @@ const bookmarkList = (function(){
 
   // I can see a list of my bookmarks when I first open the app
   function generateBookmarkElement(bookmark) {
-    //TODO: change template to a new version with more features
     return `<li data-item-id="${bookmark.id}" class='js-bookmark-element'>
               <div class="js-title">${bookmark.title}</div>  
               <div>${bookmark.rating}</div>
@@ -24,26 +23,15 @@ const bookmarkList = (function(){
   }
 
   function render() {
-    let items = store.items;
-    console.log('`render` ran');
-    const bookmarkListItemsString = generateBookmarkString(items);
+    const items = store.items.filter(item => item.rating >= store.minRating);
+    const bookmarkListItemsString = generateBookmarkString(items); 
     $('.js-bookmarks-list').html(bookmarkListItemsString);
   }
 
-
-  //I can click on a bookmark to display the "detailed" view -- TOD0: this is happening in the DOM, we need to put it in the store
-  function handleDetailToggle() {
-   $(".js-bookmarks-list").on("click", ".js-title", function() {
-      $(this).parent().find(".js-hide-element").toggle();
-    });
-
-  }
   // I can add bookmarks to my bookmark list.
   function toggleAddNewBookmarkForm() {
     $( "#add-button" ).click(function() {
-      console.log("add button works")
       $( ".form-container" ).toggle();
-
     });  
   }
 
@@ -54,7 +42,6 @@ const bookmarkList = (function(){
       const url = $("input[name=url]").val();
       const rating = $("input[name=rating]:checked").val();
       const description = $("textarea[name=description]").val();
-        console.log(description);
       api.createBookmark(title, url, rating, description, function (newBookmark) {
         store.addItem(newBookmark);
         render();
@@ -62,19 +49,26 @@ const bookmarkList = (function(){
     })
   };
 
+
+  //I can click on a bookmark to display the "detailed" view -- TOD0: this is happening in the DOM, we need to put it in the store
+  function handleDetailToggle() {
+    $(".js-bookmarks-list").on("click", ".js-title", function() {
+       $(this).parent().find(".js-hide-element").toggle();
+     });
+   }
+
+  // I can remove bookmarks from my bookmark list
+
   function getItemIdFromElement(item) {
     return $(item)
       .closest('.js-bookmark-element')
       .data('item-id');
   }
 
-  // I can remove bookmarks from my bookmark list
   function handleRemoveBookmark() {
     $(".js-bookmarks-list").on('click', '.js-bookmark-delete', event => {
-        
-        const id = getItemIdFromElement(event.currentTarget);
-        api.deleteBookmark(id, () => {
-         
+      const id = getItemIdFromElement(event.currentTarget);
+      api.deleteBookmark(id, () => { 
         store.findAndDelete(id);
         render();
       }, () => {
@@ -83,22 +77,27 @@ const bookmarkList = (function(){
     });
   }
   
-
-
   // I can select from a dropdown a "minimum rating" to filter the list by all bookmarks rated above the chosen selection
   function handleRatingFilter() {
+    $('.js-filter').on('click', event => {
+      const val = $(event.currentTarget).val();
+      store.minRating = val;
+      render();
+    });
 
   }
 
+  //I can edit the rating and description of a bookmark in my list
 
 
     function bindEventListeners() {
       
       toggleAddNewBookmarkForm();
-      handleDetailToggle();
-      handleRatingFilter();
       handleSubmitBookmark();
+      handleDetailToggle();
       handleRemoveBookmark();
+      handleRatingFilter();
+
 
     }
 
